89bdc14dc0bd69f2c6cc7632b2eb6237c4849348
hong yuseok <jericoh@naver.com>
2021-06-21 04:16:31 +0900

    chapter03 게시글 등록 api & test 코드

---
+++ src/main/java/com/springboot_board/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.springboot_board.springboot.service.posts;
+
+import com.springboot_board.springboot.domain.posts.PostsRepository;
+import com.springboot_board.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/springboot_board/springboot/web/PostsApiController.java
@@ -0,0 +1,19 @@
+package com.springboot_board.springboot.web;
+
+import com.springboot_board.springboot.service.posts.PostsService;
+import com.springboot_board.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
