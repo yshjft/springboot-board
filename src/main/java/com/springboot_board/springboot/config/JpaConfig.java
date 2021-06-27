551b926ed1d5f1084fd34da0909cf3e7159700f5
hong yuseok <jericoh@naver.com>
2021-06-28 04:45:18 +0900

    chapter5 완료

---
+++ src/main/java/com/springboot_board/springboot/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.springboot_board.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig { }
--- src/main/java/com/springboot_board/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/springboot_board/springboot/config/auth/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class securityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
