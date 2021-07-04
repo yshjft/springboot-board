6a76902ff7540d7217cc1ece8f7e67f39e3b3a6e
hong yuseok <jericoh@naver.com>
2021-07-04 20:28:34 +0900

    edit deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> 현재 구동 중인 어플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -fl springboot_board | grep jar | awk '{print $1}')
 
-echo "> 횬쟈 구동 중인 엎플리케이션 pid: $CURRENT_PID"
+echo "> 현재 구동 중인 엎플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
         echo "> 현재 구동 중인 어플리케이션이 없으므로 종료하지 않습니다"
 else
