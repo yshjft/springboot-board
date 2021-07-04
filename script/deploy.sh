d68647f12dfdcab692fdddbc84bf2ccfa2e36b80
hong yuseok <jericoh@naver.com>
2021-07-04 20:58:33 +0900

    edit deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 어플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl springboot_board | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot_board | grep java | awk '{print $1}')
 
 echo "> 현재 구동 중인 엎플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
