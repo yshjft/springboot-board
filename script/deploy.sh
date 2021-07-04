7e916eb5ea4292e8f842cfad6dcce1412bab138c
hong yuseok <jericoh@naver.com>
2021-07-04 22:11:49 +0900

    edit deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=springboot_board
 
 echo "> Build 파일 복사"
-sudo cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 어플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -fl springboot_board | grep java | awk '{print $1}')
@@ -24,7 +24,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo "> JAR_NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행 권한 추가"
-sudo chmod +x $JAR_NAME
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
