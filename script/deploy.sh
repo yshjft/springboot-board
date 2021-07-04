113e4270b761e98af071ae8515df0bba1baf206a
hong yuseok <jericoh@naver.com>
2021-07-04 23:52:35 +0900

    왜 안될까

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,12 +4,15 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=springboot_board
 
 echo "> Build 파일 복사"
-sudo cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 어플리케이션 pid 확인"
+
 CURRENT_PID=$(pgrep -fl springboot_board | grep java | awk '{print $1}')
 
 echo "> 현재 구동 중인 엎플리케이션 pid: $CURRENT_PID"
+
 if [ -z "$CURRENT_PID" ]; then
         echo "> 현재 구동 중인 어플리케이션이 없으므로 종료하지 않습니다"
 else
@@ -19,14 +22,17 @@ else
 fi
 
 echo "> 새 어플리케이션 배포"
+
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR_NAME: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행 권한 추가"
-sudo chmod +x $JAR_NAME
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
+
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
